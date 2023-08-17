9220d573dd2bee7045de099ad083eca9f82041bf
mdrobayedhasanraja2023 <142398896+mdrobayedhasanraja2023@users.noreply.github.com>
2023-08-17 15:17:20 +0600

    Add files via upload

---
+++ rajaddose.py
@@ -0,0 +1,54 @@
+import requests
+ured="\033[4;31m" 
+on_green="\033[42m"
+ired="\033[0;91m" 
+on_ired="\033[0;101m" 
+red="\033[0;31m"  
+green="\033[0;32m" 
+color_off="\033[0m"
+print(red+"""  _____                 _               ____              __  __     _   _____       _                          _   _    _                         _____       _        
+ |  __ \               | |             |  _ \         _  |  \/  |   | | |  __ \     | |                        | | | |  | |                       |  __ \     (_)       
+ | |  | | _____   _____| | ___  _ __   | |_) |_   _  (_) | \  / | __| | | |__) |___ | |__   __ _ _   _  ___  __| | | |__| | __ _ ___  __ _ _ __   | |__) |__ _ _  __ _  
+ | |  | |/ _ \ \ / / _ \ |/ _ \| '_ \  |  _ <| | | |     | |\/| |/ _` | |  _  // _ \| '_ \ / _` | | | |/ _ \/ _` | |  __  |/ _` / __|/ _` | '_ \  |  _  // _` | |/ _` | 
+ | |__| |  __/\ V /  __/ | (_) | |_) | | |_) | |_| |  _  | |  | | (_| | | | \ \ (_) | |_) | (_| | |_| |  __/ (_| | | |  | | (_| \__ \ (_| | | | | | | \ \ (_| | | (_| | 
+ |_____/ \___| \_/ \___|_|\___/| .__/  |____/ \__, | (_) |_|  |_|\__,_| |_|  \_\___/|_.__/ \__,_|\__, |\___|\__,_| |_|  |_|\__,_|___/\__,_|_| |_| |_|  \_\__,_| |\__,_| 
+                               | |             __/ |                                              __/ |                                                      _/ |       
+                               |_|            |___/                                              |___/                                                      |__/        
+                               
+                                                              
+	My  Facebook👉https://www.facebook.com/raja.maxtan
+	
+
+My Teligram👉t.me/mdrobayedhasanraja
+
+                                                                                                          My Instragam👉https://instagram.com/rajamdrobayedhasan?utm_source=qr&igshid=ZDc4ODBmNjlmNQ%3D%3D
+                                                                                                         My github1👉https://github.com/rajavau589/mdrobayedhasanraja-
+                                                                                                         My github2👉https://github.com/mdrobayedhasanraja2023    
+                                                                                                         
+                                                                                                          My Whatsapp👉 📞01826173096                                                                      
+                     	      """+color_off)
+                     	      
+
+		
+print(green+"	1:Start Ddose attak"+color_off)
+a=int(input("Type 1: "))
+b=1
+c=2
+if a==b:
+	ip = True
+	print(red+"start attack"+color_off)
+	
+
+def ip():
+	ic=str(input("Entar Victim Ip: "))
+	it=str(input("Enar Port: "))
+	for i in range(900000):
+  		print (str(i+1)+green+"sent requst"+color_off)
+	
+ip()
+
+
+
+
+	
+
